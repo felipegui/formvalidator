eaa4c6a6d79828db73255f88185e830811947f37
Felipe Guimarães <fguiengenharia@gmail.com>
2021-03-04 00:41:12 -0300

    Exibindo o erro

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,14 @@ let fagValidator = {
 
         let inputs = form.querySelectorAll('input');
 
+        fagValidator.clearErrors();
+
         for( let i=0; i<inputs.length; i++ ) {
             let input = inputs[i];
             let check = fagValidator.checkInput(input);
             if( check != true ){
                 send = false;
-                console.log(check);
+                fagValidator.showError(input, check);
             }
         }
 
@@ -44,6 +46,29 @@ let fagValidator = {
 
         return true;
     },
+    showError: (input, error) =>{
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+
+        errorElement.classList.add('error');
+
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+    clearErrors:() => {
+        let inputs = form.querySelectorAll('input');
+
+        for( let i=0; i<inputs.length; i++ ) {
+            inputs[i].style = '';
+        }
+        let errorElements = document.querySelectorAll('.error');
+
+        for( let i=0; i<errorElements.length; i++ ) {
+            errorElements[i].remove();
+        }
+    }
 };
 
 let form = document.querySelector('.fagvalidator');
